48001b9cfda0794fa4e6d5689916414183070d2d
pizzapro422 <pizzapro420@gmail.com>
2023-06-11 12:53:36 +0800

    Added movement for the sprite

--- pygame_hello_world.py
+++ pygame_hello_world.py
@@ -6,6 +6,9 @@ pygame.init()
 screen = pygame.display.set_mode((1080, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 4, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -19,11 +22,27 @@ while running:
     screen.fill("darkgreen")
 
     # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 40)
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+    if keys[pygame.K_ESCAPE]:
+        print("escape pressed. Quitting")
+        running = False
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
